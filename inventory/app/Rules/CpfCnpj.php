be5aee67dff8f5b50e034a1aab01204ddce29508
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-02-21 21:12:32 -0300

    feat: cpf/cnpj validation

---
+++ inventory/app/Rules/CpfCnpj.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class CpfCnpj implements ValidationRule
+{
+    private CpfCnpjValidator $validator;
+
+    public function __construct(CpfCnpjValidator $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $this->validator->setValor($value);
+        $attribute = strtolower($attribute);
+
+        $valid_cpf = $attribute === 'cpf' && !$this->validator->verifica_sequencia(11);
+        $valid_cnpj = $attribute === 'cnpj' && !$this->validator->verifica_sequencia(14);
+
+        if (!$valid_cpf || !$valid_cnpj || !$this->validator->valida($value)) {
+            $fail('The :attribute is invalid.');
+        }
+    }
+}
