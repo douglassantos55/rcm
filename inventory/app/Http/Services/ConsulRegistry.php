e99f0b93b58900a333e4b0f29e78dc40371e5d95
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-03-16 20:07:45 -0300

    refactor(inventory): service registry

--- inventory/app/Http/Services/ConsulRegistry.php
+++ inventory/app/Http/Services/ConsulRegistry.php
@@ -2,54 +2,27 @@
 
 namespace App\Http\Services;
 
-use Consul\Services\Agent;
 use Consul\Services\Catalog;
 use Illuminate\Support\Facades\Log;
 use RuntimeException;
 
 class ConsulRegistry implements Registry
 {
-    /** @var Agent */
-    private $agent;
-
     /** @var Catalog */
     private $catalog;
 
     public function __construct()
     {
-        $this->agent = new Agent();
         $this->catalog = new Catalog();
     }
 
-    public function getAddress(string $id): string
+    public function get(string $service): string
     {
         try {
-            $response = $this->catalog->service($id);
-            return $response->getBody();
+            $response = $this->catalog->service($service)->json();
+            return $response['ServiceAddress'];
         } catch (RuntimeException $ex) {
-            Log::error('could not get service: ' . $ex->getMessage(), ['id' => $id]);
-            throw $ex;
-        }
-    }
-
-    public function register(string $name): void
-    {
-        try {
-            $response = $this->agent->registerService([
-                'id' => $name,
-                'name' => $name,
-                'tags' => ['v1'],
-                'address' => env('APP_URL'),
-            ]);
-
-            if (!$response->isSuccessful()) {
-                throw new RuntimeException($response->getBody());
-            }
-        } catch (RuntimeException $ex) {
-            Log::error('could not register service: ' . $ex->getMessage(), [
-                'name' => $name,
-                'address' => env('APP_URL'),
-            ]);
+            Log::error('could not get service: ' . $ex->getMessage(), ['service' => $service]);
             throw $ex;
         }
     }
--- inventory/app/Http/Services/Registry.php
+++ inventory/app/Http/Services/Registry.php
@@ -5,18 +5,11 @@ namespace App\Http\Services;
 interface Registry
 {
     /**
-     * Registers a service
-     *
-     * @param string $name
-     */
-    public function register(string $name): void;
-
-    /**
      * Returns the service's address
      *
      * @param string $service The name of the service to look for
      *
      * @return string The address of the service
      */
-    public function getAddress(string $service): string;
+    public function get(string $service): string;
 }
