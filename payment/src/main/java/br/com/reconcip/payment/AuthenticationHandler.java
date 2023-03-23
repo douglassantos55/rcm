65b6cb919e1c177048be2d9c08e2cf5d41b05904
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-03-22 21:57:39 -0300

    feat(payment): authentication

---
+++ payment/src/main/java/br/com/reconcip/payment/AuthenticationHandler.java
@@ -0,0 +1,58 @@
+package br.com.reconcip.payment;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class AuthenticationHandler {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.issuer}")
+    private String expectedIssuer;
+
+    @Value("${jwt.audience}")
+    private String expectedAudience;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec(this.secret.getBytes(), JwsAlgorithms.HS256);
+        NimbusJwtDecoder decoder = NimbusJwtDecoder.withSecretKey(secretKey).build();
+
+        decoder.setJwtValidator(jwtValidator());
+        return decoder;
+    }
+
+    public OAuth2TokenValidator<Jwt> jwtValidator() {
+        return new DelegatingOAuth2TokenValidator(
+                new JwtTimestampValidator(),
+                new JwtIssuerValidator(expectedIssuer),
+                new JwtClaimValidator<List<String>>(
+                        JwtClaimNames.AUD,
+                        aud -> aud != null && aud.contains(expectedAudience)
+                )
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .build();
+    }
+}
