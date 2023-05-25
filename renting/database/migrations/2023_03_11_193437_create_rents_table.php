435e0a85d866257af78ee08d841786156ffe5f37
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-05-25 18:42:22 -0300

    feat(renting): add rent number

--- renting/database/migrations/2023_03_11_193437_create_rents_table.php
+++ renting/database/migrations/2023_03_11_193437_create_rents_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('rents', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->unsignedInteger('number')->unique();
             $table->dateTime('start_date');
             $table->dateTime('end_date');
             $table->uuid('payment_type_id');
@@ -34,6 +36,8 @@ return new class extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        DB::statement('ALTER TABLE rents CHANGE number number INT UNSIGNED AUTO_INCREMENT');
     }
 
     /**
