c24db7f997014c1c076a0b6193dfeaf9f8824d5b
Douglas Santos <hiperhavy_doug@hotmail.com>
2023-06-04 16:22:57 -0300

    feat(accounting): env + wiring

---
+++ accounting/cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"reconcip.com.br/accounting/pkg"
+)
+
+func main() {
+	driver := pkg.MySQL{
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Database: os.Getenv("DB_DATABASE"),
+	}
+
+	repository, err := pkg.NewSqlRepository(driver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc := pkg.NewService(repository)
+	if err := pkg.OrderCreatedSubscriber(svc, "rabbitmq", "guest", "guest"); err != nil {
+		log.Print(err)
+	}
+}
